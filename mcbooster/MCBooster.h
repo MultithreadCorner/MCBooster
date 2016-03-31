864348448f49ffb5d70ad8a9e5b0e0ade78b3a12
Augusto <augalves@LabHome>
2016-03-31 17:11:42 +0200

    Add doxygen main page to mcbooster/MCBooster.h

--- mcbooster/MCBooster.h
+++ mcbooster/MCBooster.h
@@ -2,3 +2,15 @@
 #define MCBooster_VERSION_MAJOR 1
 #define MCBooster_VERSION_MINOR 0
 #define MCBooster_VERSION_PATCH 0
+
+/**
+\mainpage MCBooster
+
+What is it?
+-----------
+  MCBooster is an header only library designed for the fast generation of
+  phase space events. The library make use of thrust library and can deploy OpenMP
+  threads, CUDA and Xeon Phi cores.
+  It is focused on performance and precision.  
+
+*/
